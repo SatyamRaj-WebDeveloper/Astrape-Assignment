53be7714b81cd098473a4db245af1e55962dabc7
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2025-09-07 18:39:57 +0530

    final commit

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const PORT  = process.env.PORT ;
 dbConnect()
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL, // Next.js runs on 3000
+  origin: "http://localhost:3000", // Next.js runs on 3000
   credentials: true
 }));
 
