e0ec2ae2d97a391b4d5de976ee3b0f7fd96b473f
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2025-09-07 19:34:04 +0530

    minor change

--- frontend/src/app/admin/page.js
+++ frontend/src/app/admin/page.js
@@ -1,6 +1,6 @@
 "use client"
 import FilterSidebar from "../components/FilterSidebar";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 export default function AdminPage() {
   const [items, setItems] = useState([]);
